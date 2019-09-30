84d10eedf5de368adf93c04dbfa7d09695487222
Lenard Frommelt <lenny09918050@googlemail.com>
2019-09-30 08:01:13 +0200

    thingyfied the editor frontend

---
+++ webpack-watch.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "mode": "production",
+    "devtool": "none",
+    "entry": "/home/lenny/gits/program-editor-frontend/toolset/compiled/js/index.js",
+    "output": {
+        "filename": "bundle.js",
+        "path": "/home/lenny/gits/program-editor-frontend/toolset/compiled"
+    },
+    "watch": true
+}
